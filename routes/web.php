8639a027adcf09d70ea611b8580db5f689034655
Owen Selles <owenselles@icloud.com>
2022-04-06 09:51:27 +0200

    add home page

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('/home/welcome');
 });
 
 Route::resource('/catalog/facilities', FacilitiesController::class);
