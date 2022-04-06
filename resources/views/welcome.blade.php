48760528bd26b186fa7cbc710f4bdd6e89eea9ba
Owen Selles <owenselles@icloud.com>
2022-04-06 09:48:00 +0200

    add simple home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,15 +6,40 @@
         System <strong>/XX/</strong> is down! Click <a>here</a> to read more.
     </div>
 
-    <div class="columns is-mobile is-centered">
-        <div class="column is-half">
-            <p class="bd-notification is-primary">
-                <code class="html">is-half</code><br>
-            </p>
+    <section class="container mt-6">
+        <div class="columns is-multiline is-centered">
+            <div class="column is-10 register">
+                <div class="columns">
+                    <div class="column left">
+                        <h1 class="title is-1">Hello 'Name',</h1>
+                        <h2 class="subtitle colored is-4">Today you have the following activities planned:</h2>
+                        <p>11:00 - Meeting with CEO</p>
+                        <p>12:00 - Lunch</p>
+                    </div>
+                    <div class="column right has-text-centered">
+                        <h1 class="title is-4">Apps</h1>
+                        <form>
+                            <div class="box">
+                                <p class="has-text-left has-text-weight-bold">Your recently used apps:</p>
+                                <div class="control">
+                                    <p class="has-text-left mt-4 is-underlined">Request new laptop</p>
+                                    <p class="has-text-left is-underlined">File a exemption</p>
+                                    <p class="has-text-left is-underlined">Create a ticket</p>
+                                </div>
+                            </div>
+
+                            <div class="box">
+                                <p class="has-text-left has-text-weight-bold">Your favorite apps:</p>
+                                <div class="control">
+                                    <p class="has-text-left mt-4 is-underlined">Order lunch</p>
+                                    <p class="has-text-left is-underlined">Change password</p>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
-    </div>
-
-
-
+    </section>
 @endsection
 
