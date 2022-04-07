530652a16abda3cde432d2705255fcd4f24c326a
Naomi674 <norr0001@hz.nl>
2022-04-07 11:01:22 +0200

    Added a page for My Tickets

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\FacilitiesController;
+use App\Http\Controllers\TicketController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,3 +20,4 @@ Route::get('/', function () {
 });
 
 Route::resource('/catalog/facilities', FacilitiesController::class);
+Route::resource('/ticket', TicketController::class);
