7107e43ea431e9349f191f9b81d9607ebe4df120
Pollygan <90186074+Pollygan@users.noreply.github.com>
2022-05-10 15:00:24 +0300

    fixed the some styling of the system status page

--- resources/views/status.blade.php
+++ resources/views/status.blade.php
@@ -15,40 +15,34 @@
         </div>
     </div>
     <div class="container mt-5 p-6">
-            <div class="columns">
-            <div class="column is-one"><div class="card">
+        <div class="columns">
+            <div class="column is-one">
+                <div class="card">
                     <ul>
-                        <li class="title is-5 p-2" style="background: #e35205">Legend</li>
-                        <li>
-                            <span class="icon">
-                      <i class="fa-solid fa-circle-check"></i>
-                    </span>
+                        <li class="has-text-white is-5 p-2" style="background: #e35205">Legend</li>
+                        <li class="p-1">
+                            <i class="fa-solid fa-circle-check"></i>
                             No issues
                         </li>
-                        <li>
-                            <span class="icon">
-                      <i class="fa-solid fa-circle-info"></i>
-                    </span>
+                        <li class="p-1">
+                            <i class="fa-solid fa-circle-info"></i>
                             Planned maintenance
                         </li>
-                        <li><span class="icon">
-                      <i class="fa-solid fa-circle-minus"></i>
-                    </span>
+                        <li class="p-1">
+                            <i class="fa-solid fa-circle-minus"></i>
                             Service degradation
                         </li>
-                        <li><span class="icon">
-                     <i class="fa-solid fa-circle-exclamation"></i>
-                    </span>
+                        <li class="p-1">
+                            <i class="fa-solid fa-circle-exclamation"></i>
                             Outage
                         </li>
-                        <li>
-                            <span class="icon">
-                     <i class="fa-solid fa-circle-plus"></i>
-                    </span>
+                        <li class="p-1">
+                            <i class="fa-solid fa-circle-plus"></i>
                             Multiple issues
                         </li>
                     </ul>
-            </div></div>
+                </div>
+            </div>
             <div class="column is-four-fifths">
                 <table class="table is-hoverable is-fullwidth ml-auto mr-auto ">
                     <thead>
@@ -91,102 +85,66 @@
                     <tr>
                         <th>Digital Workplace</th>
                         <th>
-                            <span class="icon">
-                              <i class="fa-solid fa-envelope"></i>
-                            </span>
+                            <i class="fa-solid fa-envelope"></i>
                         </th>
                         <th>
-                            <span class="icon">
-                              <i class="fa-solid fa-circle-check"></i>
-                            </span>
+                            <i class="fa-solid fa-circle-check"></i>
                         </th>
                         <th>
-                            <span class="icon">
-                              <i class="fa-solid fa-circle-check"></i>
-                            </span>
+                            <i class="fa-solid fa-circle-check"></i>
                         </th>
                         <th>
-                <span class="icon">
-                  <i class="fa-solid fa-circle-check"></i>
-                </span>
+                            <i class="fa-solid fa-circle-check"></i>
                         </th>
                         <th>
-                <span class="icon">
-                  <i class="fa-solid fa-circle-check"></i>
-                </span>
+                            <i class="fa-solid fa-circle-check"></i>
                         </th>
                         <th>
-                <span class="icon">
-                  <i class="fa-solid fa-circle-check"></i>
-                </span>
+                            <i class="fa-solid fa-circle-check"></i>
                         </th>
                         <th>Operational</th>
                     </tr>
                     <th>E-Commerce</th>
                     <th>
-                <span class="icon">
-                  <i class="fa-solid fa-envelope"></i>
-                </span>
+                        <i class="fa-solid fa-envelope"></i>
                     </th>
                     <th>
-                <span class="icon">
-                 <i class="fa-solid fa-circle-check"></i>
-                </span>
+                        <i class="fa-solid fa-circle-check"></i>
                     </th>
                     <th>
-                <span class="icon">
-                 <i class="fa-solid fa-circle-check"></i>
-                </span>
+                        <i class="fa-solid fa-circle-check"></i>
                     </th>
                     <th>
-                <span class="icon">
-                  <i class="fa-solid fa-circle-check"></i>
-                </span>
+                        <i class="fa-solid fa-circle-check"></i>
                     </th>
                     <th>
-                <span class="icon">
-                  <i class="fa-solid fa-circle-check"></i>
-                </span>
+                        <i class="fa-solid fa-circle-check"></i>
                     </th>
                     <th>
-                <span class="icon">
-                  <i class="fa-solid fa-circle-check"></i>
-                </span>
+                        <i class="fa-solid fa-circle-check"></i>
                     </th>
                     <th>Operational</th>
                     </tr>
-
                     <tr>
                         <th>Files & Folders</th>
                         <th>
-                <span class="icon">
-                  <i class="fa-solid fa-envelope"></i>
-                </span>
+                            <i class="fa-solid fa-envelope"></i>
                         </th>
                         <th>
-                <span class="icon">
-                  <i class="fa-solid fa-circle-check"></i>
-                </span>
+
+                            <i class="fa-solid fa-circle-check"></i>
                         </th>
                         <th>
-                <span class="icon">
-                  <i class="fa-solid fa-circle-check"></i>
-                </span>
+                            <i class="fa-solid fa-circle-check"></i>
                         </th>
                         <th>
-                <span class="icon">
-                  <i class="fa-solid fa-circle-check"></i>
-                </span>
+                            <i class="fa-solid fa-circle-check"></i>
                         </th>
                         <th>
-                <span class="icon">
-                  <i class="fa-solid fa-circle-check"></i>
-                </span>
+                            <i class="fa-solid fa-circle-check"></i>
                         </th>
                         <th>
-                <span class="icon">
-                 <i class="fa-solid fa-circle-check"></i>
-                </span>
+                            <i class="fa-solid fa-circle-check"></i>
                         </th>
                         <th>Operational</th>
                     </tr>
