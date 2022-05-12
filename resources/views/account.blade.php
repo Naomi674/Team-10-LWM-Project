c8cefdf9f932a8bd0182c22794b430e53aa9df37
LifeAdmin-cmd <lars.kra2001@gmail.com>
2022-05-12 10:52:04 +0200

    fixed account css

--- resources/views/account.blade.php
+++ resources/views/account.blade.php
@@ -1,33 +1,53 @@
 @extends('Components.layout')
 
 @section('content')
-    <div class="container">
+    <div class="container mt-5">
         <div class="columns is-centered">
             <div class="column is-5-tablet is-4-desktop is-3-widescreen">
                 <form method="POST" action="{{ route('login') }}" class="box">
                     @csrf
                     <div class="field">
-                        <label class="">
-                            <p class="label">Name</p>
-                            <input class="input" type="text" value="{{ auth()->user()->name }}">
-                        </label>
+                        <label class="label" for="name">Name</label>
+                        <div class="control has-icons-left">
+                            <input class="input" type="text" name="name" value="{{ auth()->user()->name }}">
+                            <span class="icon is-small is-left">
+                                <i class="fa-solid fa-id-card"></i>
+                            </span>
+                        </div>
                     </div>
                     <div class="field">
-                        <label>
-                            <p class="label">Email</p>
-                            <input class="input" type="email" value="{{ auth()->user()->email }}">
-                        </label>
+                        <label class="label">Email</label>
+                            <div class="control has-icons-left">
+                                <input class="input" type="email" value="{{ auth()->user()->email }}">
+                                <span class="icon is-small is-left">
+                                    <i class="fa-solid fa-envelope"></i>
+                                </span>
+                            </div>
                     </div>
                     <div class="field">
                         <label>
                             <p class="label">Password</p>
-                            <p class="input">***********</p>
+                            <div class="control has-icons-left">
+                                <p class="input">***********</p>
+                                <span class="icon is-small is-left">
+                                    <i class="fa-solid fa-lock"></i>
+                                </span>
+                            </div>
                         </label>
                     </div>
                     <div class="field">
                         <label>
                             <p class="label">Role</p>
-                            <p class="input">{{ auth()->user()->role_id == 0 ? 'User' : 'Admin' }}</p>
+                            <div class="control has-icons-left">
+                                <p class="input">{{ auth()->user()->role_id == 0 ? 'User' : 'Admin' }}</p>
+                                <span class="icon is-small is-left">
+                                    @if(auth()->user()->role_id == 0)
+                                        <i class="fa-solid fa-user"></i>
+                                    @else
+                                        <i class="fa-solid fa-wrench"></i>
+                                    @endif
+                                </span>
+                            </div>
                         </label>
                     </div>
                 </form>
