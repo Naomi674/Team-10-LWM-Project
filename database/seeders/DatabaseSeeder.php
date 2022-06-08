394274bbaff2dd18637683c512266f2eafaa0f67
LifeAdmin-cmd <lars.kra2001@gmail.com>
2022-06-08 11:40:38 +0200

    hotfixed users db

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,5 +16,6 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         $this->call(KnowledgeSeeder::class);
+        $this->call(UserSeeder::class);
     }
 }
