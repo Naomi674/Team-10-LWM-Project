289a669bc84cca49842c93fc3f9ce2b9d7f1a4c9
LifeAdmin-cmd <lars.kra2001@gmail.com>
2022-06-26 12:55:03 +0200

    refactored userManagement

--- resources/views/admin/userManagement.blade.php
+++ resources/views/admin/userManagement.blade.php
@@ -101,7 +101,7 @@
                                 <div class="field">
                                     <label class="label">Password</label>
                                     <p class="control has-icons-left">
-                                        <input class="input" type="password" name="password" placeholder="{{ $user->password }}">
+                                        <input class="input" type="password" name="password" placeholder="Type new password">
                                         <span class="icon is-small is-left">
                                           <i class="fas fa-lock"></i>
                                         </span>
